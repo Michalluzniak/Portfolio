956fa9002296a74b3404807f8233c6b13af72e2b
Michał Łuźniak <michal.luzniak8@gmail.com>
2023-04-10 20:15:27 +0200

    feat(layout): add background images to created layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,19 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      gridTemplateColumns: {
+        layout: '5rem 1fr 5rem',
+      },
+      gridTemplateRows: {
+        layout: '5rem 1fr 5rem',
+      },
+      colors: {
+        'soft-white': '#F6F4F3',
+        dark: '#020202',
       },
     },
   },
   plugins: [],
-}
+};
