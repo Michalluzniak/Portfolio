92a66b48f060bd3a659effd296ccb667e2343fc5
Michał Łuźniak <michal.luzniak8@gmail.com>
2023-04-19 15:54:13 +0200

    fix(scrollIcon): change scroll icon position

--- src/components/Slider/ScrollIcon.tsx
+++ src/components/Slider/ScrollIcon.tsx
@@ -3,7 +3,7 @@ import { BsFillArrowDownCircleFill } from 'react-icons/bs';
 
 export const ScrollIcon = () => {
   return (
-    <div className="lg:right-20 lg:top-2/3 xl:right-14 2xl:right-24 flex absolute top-5">
+    <div className="lg:right-20 lg:top-2/3 xl:right-14 xl:top-5 2xl:right-24 flex absolute">
       <div className="flex flex-col  items-center scroll-icon-container opacity-0 ">
         <BsFillArrowDownCircleFill className="lg:text-xl xl:text-2xl text-soft-white  mb-4 scroll-icon" />
         <p className="text-soft-white text-xs uppercase weight-300 tracking-wide scroll-icon-description">
--- src/modules/Sections/AboutMe/index.tsx
+++ src/modules/Sections/AboutMe/index.tsx
@@ -9,14 +9,14 @@ const AboutMe = (animationLoadingHandler: any) => {
   return (
     <section className="grid grid-cols-3 grid-rows-section h-full w-full shrink-0 sections" id="about-me-section">
       <div className="col-start-1 row-start-1 text-soft-white ms-24 self-center hero-name font-semibold translate-x-[-150%]">
-        <h1 className="lg:text-3xl lg:w-64 xl:text-4xl font-main">Michał Łuźniak</h1>
-        <h2 className="text-xl font-second ms-0.5">Front-End Developer</h2>
+        <h1 className="lg:text-3xl xl:text-4xl 2xl:text-6xl w-[200%] font-normal  font-main">Michał Łuźniak</h1>
+        <h2 className="lg:text-xl 2xl:text-3xl font-normal  font-second ms-0.5">Front-End Developer</h2>
       </div>
-      <div className="lg:w-56 lg:h-56 xl:w-60 xl:h-60 col-start-1 row-start-2 backdrop-blur-md w-72 h-72 border border-soft-white ms-24">
+      <div className="lg:w-56 lg:h-56 xl:w-60 xl:h-60 2xl:w-72 2xl:h-72 col-start-1 row-start-2 backdrop-blur-md w-72 h-72 border border-soft-white ms-24">
         <Image src={profilePhoto} fill alt="background cherry blossom picture"></Image>
       </div>
 
-      <div className="about-me lg:w-4/6 xl:w-4/6  2xl:w-2/6 col-start-3 row-start-2 row-span-2 justify-self-end self-start me-10 text-soft-white">
+      <div className="about-me lg:w-4/6 xl:w-3/6  2xl:w-2/6 2xl:row-start-3  col-start-3 row-start-2  justify-self-end self-start me-10 text-soft-white">
         <p className="font-main lg:text-xs  xl:text-xs text-sm tracking-wide hero-description">
           <span id="hero-description-content"></span>
         </p>
